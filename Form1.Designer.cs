435765bfff3e1009c895d8fe0571df4cca479f9e
79152 <7915241@gmail.com>
2023-03-28 15:30:13 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace knut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.элемент = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 69);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сгенерировать случайные элементы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // элемент
+            // 
+            this.элемент.HeaderText = "элемент";
+            this.элемент.Name = "элемент";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "номер элемента";
+            this.Column1.Name = "Column1";
+            // 
+            // table
+            // 
+            this.table.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.элемент});
+            this.table.GridColor = System.Drawing.SystemColors.ControlLightLight;
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(239, 588);
+            this.table.TabIndex = 4;
+            this.table.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(414, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 69);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Рассчитать коэфициент";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn элемент;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        public System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace knut
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//rand
+        {
+            int[] numbers = new int[20];
+            int[] Array = new int[20];
+            Random rnd1 = new Random();
+
+            for (int i = 0; i < 20; i++)
+                table.Rows.Add();
+
+            for (int i = 0; i < 1; i++)
+                table.Columns.Add(i.ToString(), i.ToString());
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    numbers[i] = i + 1;
+                    table.Rows[i].Cells[0].Value = numbers[i];
+                    table.Rows[i].Cells[1].Value = rnd1.Next(0, 99);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)//coef
+        {
+            string[] a = new string[table.RowCount];
+            int[] b = new int[table.RowCount];
+
+            int sum_el = 0;
+            int sum_el2 = 0;
+            int first_coef = 0; 
+            int second_coef = 0;
+
+            for (int i = 0; i < table.RowCount - 1; i++)
+            {
+                a[i] = table[1, i].Value.ToString();
+                int.TryParse(a[i], out b[i]);
+            }
+
+           for (int i = 0; i < b.Length; i++)
+               sum_el += b[i];
+
+            first_coef = sum_el / b.Length;
+
+           for (int i = 0;i < b.Length; i++)
+                sum_el2 += Math.Abs(b[i] - first_coef);
+
+            second_coef = sum_el2 / b.Length;
+
+            
+            MessageBox.Show(string.Format(Convert.ToString(second_coef).PadLeft(2, '0') + " "), "Коэффициент:");
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+/* double sum = 0, sum2 = 0, sumShifted = 0;
+            double first = GetNext(0);
+            double prev = first;
+            for (int i = 0; i < n; i++)
+            {
+                double next = i == (n - 1) ? first : GetNext(i + 1);
+
+                sum += prev;
+                sum2 += prev * prev;
+                sumShifted += prev * next;
+
+                prev = next;
+            }
+            double coeff = (n * sumShifted - sum * sum) / (n * sum2 - sum * sum);*/
+
+
+/*int[] numbers_vvod = new int[20];
+           int[] num = new int[50];
+
+          for (int i = 0; i < textBox1.TextLength; i++)
+              numbers_vvod[i] = int.Parse(textBox1.ToString());
+
+           for (int i = 0; i < numbers_vvod.Length; i++)
+               table.Rows.Add();
+
+           for (int i = 0; i < 1; i++)
+               table.Columns.Add(i.ToString(), i.ToString());
+
+           for (int i = 0; i < numbers_vvod.Length; i++)
+           {
+               for (int j = 0; j < 2; j++)
+               {
+                   num[i] = i + 1;
+                   table.Rows[i].Cells[0].Value = num[i];
+                   table.Rows[i].Cells[1].Value = numbers_vvod[i];
+               }
+           }*/
